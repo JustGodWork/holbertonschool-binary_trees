55df06b53d98400f0cb3305c69b01124ef7eb426
GillesR76 <gilles-richard76@hotmail.com>
2024-01-08 02:07:53 +0100

    implementing the function to create a binary tree node

--- binary_tree.h
+++ binary_tree.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,6 +26,8 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
