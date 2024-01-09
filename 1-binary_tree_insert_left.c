6e7e1e5f081467027ec70368375269914b3ec3de
Gilles Richard <gilles-richard76@hotmail.com>
2024-01-09 03:43:03 -0800

    adding the null condition to parent if null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
